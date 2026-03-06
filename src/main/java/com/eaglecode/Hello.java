185c796f023f9ae7d0daa05ab65b6bcf1afc4ed4
Adjei Andrews <andrewsadjei777@gmail.com>
2026-03-06 13:19:27 +0000

    Dependency Injection-Spring Boot

---
+++ src/main/java/com/eaglecode/Hello.java
@@ -0,0 +1,12 @@
+package com.eaglecode;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Hello {
+
+
+    public void greet() {
+        System.out.println("I feel good!");
+    }
+}
